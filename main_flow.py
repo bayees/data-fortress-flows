3206c4195a2cf710ff1c7d63f4da68b22e2d12e1
extchba <chba@kapacity.dk>
2023-05-09 20:58:23 +0200

    Added action

---
+++ main_flow.py
@@ -0,0 +1,16 @@
+import requests
+from prefect import flow, task
+
+@task
+def call_api(url):
+    response = requests.get(url)
+    print(response.status_code)
+    return response.json()
+
+@flow
+def main_flow(url):
+    fact_json = call_api(url)
+    return fact_json
+
+if __name__ == "main":
+    print(api_flow("https://catfact.ninja/fact"))
