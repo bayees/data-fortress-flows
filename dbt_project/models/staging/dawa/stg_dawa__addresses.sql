59af447381a93e3db6e8d41e09cfa3668a399ab7
extchba <chba@kapacity.dk>
2023-05-30 21:59:28 +0200

    dawa

--- dbt_project/models/staging/dawa/stg_dawa__addresses.sql
+++ dbt_project/models/staging/dawa/stg_dawa__addresses.sql
@@ -1,11 +1,16 @@
-
+{{
+  config(
+    materialized='incremental',
+    unique_key="longitude_degrees || '-' || latitude_degrees"
+  )
+}}
 
 with source as (
       select * from {{ source('external_source', 'dawa__addresses') }}
 ),
 renamed as (
-    select
-        *
-    from source
+  select
+    *
+  from source
 )
 select * from renamed
