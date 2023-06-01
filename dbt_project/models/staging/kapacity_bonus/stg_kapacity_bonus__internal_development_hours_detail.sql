01dfd51e85c31ea6949d968683aecd8da2eaf74d
extchba <chba@kapacity.dk>
2023-06-01 19:37:36 +0200

    update

--- dbt_project/models/staging/kapacity_bonus/stg_kapacity_bonus__internal_development_hours_detail.sql
+++ dbt_project/models/staging/kapacity_bonus/stg_kapacity_bonus__internal_development_hours_detail.sql
@@ -1,3 +1,10 @@
+{{
+  config(
+    materialized='incremental',
+    unique_key="object_name"
+  )
+}}
+
 with source as (
       select * from {{ source('external_source', 'kapacity_bonus__internal_development_hours_detail') }}
 ),
