fcc8e0aa35d605fb41fef92ae17ae6647a11a9df
extchba <chba@kapacity.dk>
2023-07-12 17:41:34 +0200

    string split

--- prefect_project/tasks/generic_tasks.py
+++ prefect_project/tasks/generic_tasks.py
@@ -20,7 +20,8 @@ def write_raw(df: pd.DataFrame, path: str, columns: list = []) -> None:
     # Adding empty dataframe to enforce columns
     empty = pd.DataFrame(columns=columns)
 
-    df.to_pickle(f"./{path}.pkl")
+    print(path)
+    df.to_pickle(f"./{path.split('/')[-1]}.pkl")
     df = pd.concat([empty, df], ignore_index=True)
     with duckdb.connect() as con:
         con.sql("INSTALL httpfs")
