eef9ffe84ada5bff1074a9e5270ceb14352f245a
extchba <chba@kapacity.dk>
2023-07-12 16:02:57 +0200

    new path

--- prefect_project/tasks/extract__dawa.py
+++ prefect_project/tasks/extract__dawa.py
@@ -67,7 +67,7 @@ def transform(responses: list) -> pd.DataFrame:
         dfs.append(df)
     df = pd.concat(dfs)
     df.columns = df.columns.str.replace(r"[().]", "_", regex=True)
-    df.to_pickle("dump.pkl")
+    df.to_pickle("/opt/prefect/dump.pkl")
     return df
 
 @flow
